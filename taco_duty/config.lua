f23b4c2a6c8303dfdab89179b9e5e4ef35e9115c
Taco <79993302+Just-Taco@users.noreply.github.com>
2023-05-10 01:54:38 +0200

    Update config.lua

--- taco_duty/config.lua
+++ taco_duty/config.lua
@@ -6,7 +6,7 @@
 Config = {}
 
 Config.NPC = {
-  {442.78607177734,-973.84216308594,25.668556213379,"Police",180.5,0xA1435105,"a_m_y_business_03"}, -- Police Station 
+  {442.78607177734,-973.84216308594,25.668556213379,"Police",180.5,0xA1435105,"a_m_y_business_03"}, -- Police Station   (((Alle spillere kan bruge npcen vis de har et job så man sætter bare npcer hvor der er arbejde)))
 }
 
 Config.jobtypes = {"Politi-Job", "EMS-Job"} -- MAX 4 typer eller skriv til mig... TacoTheDev#2615              ##### Vis du skal finde andre typer så er det vrp = cfg = groups.lua = også er det bare det hvor der står gtype
